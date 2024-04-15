25da82bfe40efd5185bac65758e930dd02ada277
m-nt-s-no <45675504+m-nt-s-no@users.noreply.github.com>
2024-04-15 13:16:00 +0000

    initial commit, comments only

---
+++ app.rb
@@ -0,0 +1,20 @@
+#initialize account with $10,000 balance
+#0 stocks in account
+#list of ten fake stocks with prices
+
+#buy method:
+#make sure stock being bought matches company names or ticker symbols
+#make sure buy amount is less than or equal to account balance
+#reduce balance by correct amount
+
+#sell method:
+#make sure stock being sold is in user's account
+#credit balance with $ corresponding to stocks sold
+
+#update_prices method:
+#make sure price doesn't go below $1
+
+#loop: 
+#show balance
+#offer 3 options: buy, sell, exit
+#at end of each loop iteration, call update_prices
