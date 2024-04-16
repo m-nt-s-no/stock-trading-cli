5852a3e7bbea67167a50072bd9ddcbaa3098a88d
m-nt-s-no <45675504+m-nt-s-no@users.noreply.github.com>
2024-04-16 13:28:36 +0000

    wrote initiatlize and update_prices methods; still need testing

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
+Class StockApp
+  attr_accessor :balance, :account, :stocks
+
 #initialize account with $10,000 balance
 #0 stocks in account
 #list of ten fake stocks with prices
+  def initialize
+    @balance = 10000
+    @account = []
+    @stocks = {"Globo Bank"=> {"ticker"=> "GB", "price"=> 200, "variance"=> [-8, 10]},
+               "TechTastic"=> {"ticker"=> "TT", "price"=> 500, "variance"=> [-40, 50]},
+               "OldSkool Corp"=> {"ticker"=> "OSC", "price"=> 100, "variance"=> [-1, 2]},
+               "Tasty Treats Inc"=> {"ticker"=> "TTI", "price"=> 50, "variance"=> [-5, 5]},
+               "FlyByNight Airlines"=> {"ticker"=> "FBN", "price"=> 125, "variance"=> [-10, 8]},
+               "Medical MumboJumbo"=> {"ticker"=> "MMJ", "price"=> 400, "variance"=> [-80, 80]},
+               "Equestrian Coffee Co"=> {"ticker"=> "TT", "price"=> 40, "variance"=> [-2, 3]},
+               "Max's Flying Cars"=> {"ticker"=> "MAX", "price"=> 250, "variance"=> [-50, 40]},
+               "Funtime Toys"=> {"ticker"=> "FUN", "price"=> 75, "variance"=> [-5, 5]},
+               "Specific Dynamics"=> {"ticker"=> "TT", "price"=> 150, "variance"=> [-10, 10]},
+  }
 
 #buy method:
 #make sure stock being bought matches company names or ticker symbols
@@ -11,8 +28,12 @@
 #make sure stock being sold is in user's account
 #credit balance with $ corresponding to stocks sold
 
-#update_prices method:
-#make sure price doesn't go below $1
+  def update_prices
+  #make sure price doesn't go below $1
+  self.stocks.each do |stock|
+    key, val = stock
+    a, b = val["variance"]
+    val["price"] = val["price"] + rand(a..b)
 
 #loop: 
 #show balance
